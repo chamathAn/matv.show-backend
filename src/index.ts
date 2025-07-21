2afab8cc535b8043877860a7acd2521f88488349
Chamath Anjula <chamathanjula2@gmail.com>
2025-07-21 22:00:46 +0530

    feat: server better-auth with google complete setup

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import express, { Application, Request, Response } from "express";
 
 const app: Application = express();
-const port = 8080;
+const port = 3000;
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hlo my World!");
