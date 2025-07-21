e3209a30268b5e5c9501523f03f53e2f3580d4ed
Chamath Anjula <chamathanjula2@gmail.com>
2025-07-21 10:31:12 +0530

    chore: test eslint, prettier, husky and lint-staged

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
-import express, { Application, Request, Response } from "express";
+import express,{ Application , Request, Response } from "express"; // Missing space after import
 
-const app: Application = express();
-const port = 8080;
+const app:Application = express();  // No spaces around the `:` and extra spacing after `=`
+const port =8080;  // No spaces around `=`
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello my World!");
+app.get("/",(req:Request,res:Response)=>{  // Missing spaces after commas and around `=>`
+  res.send("Hello my World!");  // No space after the `=` sign
 });
 
-app.listen(port, () => {
-  console.log(`Listening on port ${port}...`);
-});
+app.listen(port,()=>{  // No spaces after `,` and missing space around arrow function
+  console.log(`Listening on port ${port}...`)  // Missing semicolon and improper spacing
+}); 
