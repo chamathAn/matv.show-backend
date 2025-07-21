579ef7775cacf3c96cd6637d3554b3a3c1daf104
Chamath Anjula <chamathanjula2@gmail.com>
2025-07-21 23:14:38 +0530

    chore: added better-auth middleware to /api/auth/* path

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
 import express, { Application, Request, Response } from "express";
-
+import { toNodeHandler } from "better-auth/node";
+import { auth } from "./utils/auth";
 const app: Application = express();
 const port = 3000;
+
+app.all("/api/auth/{*any}", toNodeHandler(auth));
+app.use(express.json());
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hlo my World!");
